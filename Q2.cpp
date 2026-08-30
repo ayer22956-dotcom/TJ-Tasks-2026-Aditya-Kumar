e05099dc8d8c4c0a44e170b856478a5560532b6e
Adi <ayer22956@gmail.com>
2026-08-30 13:44:49 +0530

    Add Q2 file

--- Q2.cpp
+++ Q2.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
+    cout << "Enter value of n :";
     cin >> n;
 
     int arr[n];
-
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    int largest = arr[0];
-    int second = 0;
-    bool found = false;
-
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > largest) {
-            second = largest;
-            largest = arr[i];
-            found = true;
+    for (int k = 0; k < 3; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Enter no. :";
+            cin >> arr[i];
         }
-        else if (arr[i] < largest) {
-            if (!found || arr[i] > second) {
-                second = arr[i];
+        cout << endl;
+
+        int largest = arr[0];
+        int second = 0;
+        bool found = false;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] > largest)
+            {
+                second = largest;
+                largest = arr[i];
                 found = true;
             }
+            else if (arr[i] < largest)
+            {
+                if (!found || arr[i] > second)
+                {
+                    second = arr[i];
+                    found = true;
+                }
+            }
         }
-    }
 
-    if (found)
-        cout << second;
-    else
-        cout << -1;
+        if (found)
+            cout << "Second largest number is :" << second << endl
+                 << endl;
+        else
+            cout << -1 << endl;
+    }
 
     return 0;
 }
